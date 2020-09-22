ec4bfb9f01117ff372197cdd49cae89e69855d14
wnry vscode <vscode@dehai.ch>
2020-09-22 07:48:21 +0000

    easy tensorflow implemtation

--- src/app/features/facedetect/tensorflow/component/facedetect-tensorflow/facedetect-tensorflow.component.ts
+++ src/app/features/facedetect/tensorflow/component/facedetect-tensorflow/facedetect-tensorflow.component.ts
@@ -1,25 +1,21 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
-declare var cv: any;
+import { Component, ElementRef, OnInit, AfterViewInit, Renderer2, ViewChild } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 @Component({
 	selector: 'ocv-facedetect-tensorflow',
 	templateUrl: './facedetect-tensorflow.component.html',
 	styleUrls: ['./facedetect-tensorflow.component.scss'],
 })
-export class OCVFacedetectTensorFlowComponent implements OnInit {
+export class OCVFacedetectTensorFlowComponent implements OnInit, AfterViewInit {
 	@ViewChild('video', { static: true }) videoElement: ElementRef;
 	@ViewChild('canvas', { static: true }) canvas: ElementRef;
 	@ViewChild('select', { static: true }) select: ElementRef;
 
+	// Video Vars
 	videoWidth = 0;
 	videoHeight = 0;
-
-	actualCam = 0;
-
 	currentStream: any;
-
-	streaming: boolean = false;
-	openCVLoaded: boolean = false;
 
 	constraints = {
 		video: {
@@ -30,11 +26,33 @@ export class OCVFacedetectTensorFlowComponent implements OnInit {
 		},
 	};
 
+	// Model Vars
+	model: any;
+	loading: boolean;
+	predictions: [
+		{
+			className: string;
+			probability: number;
+		}
+	];
+
 	constructor(private renderer: Renderer2) {}
 
 	ngOnInit() {
 		this.startCamera();
 		this.listCameras();
+		this.loadModel();
+	}
+
+	async loadModel() {
+		this.loading = true;
+		this.model = await mobilenet.load();
+		this.loading = false;
+
+		setInterval(async () => {
+			this.predictions = await this.model.classify(this.videoElement.nativeElement);
+			await tf.nextFrame();
+		}, 5000);
 	}
 
 	startCamera() {
@@ -70,10 +88,6 @@ export class OCVFacedetectTensorFlowComponent implements OnInit {
 		this.canvas.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
 	}
 
-	toggleStream() {
-		this.streaming = !this.streaming;
-	}
-
 	listCameras() {
 		var select = this.select.nativeElement;
 		select.innerHtml = '';
@@ -107,5 +121,22 @@ export class OCVFacedetectTensorFlowComponent implements OnInit {
 			this.constraints.video.deviceId = { exact: select.value };
 		}
 		this.startCamera();
+	}
+
+	async ngAfterViewInit() {
+		/*
+		const vid = this.videoElement.nativeElement;
+
+		if (navigator.mediaDevices.getUserMedia) {
+			navigator.mediaDevices
+				.getUserMedia({ video: true })
+				.then((stream) => {
+					vid.srcObject = stream;
+				})
+				.catch((err0r) => {
+					console.log('Something went wrong!');
+				});
+		}
+		*/
 	}
 }
