26579db5f3e7f896383f40b01792d155cf10f40c
wnry vscode <vscode@dehai.ch>
2020-09-22 09:33:05 +0000

    tensorflow facedetect working

--- src/app/features/facedetect/tensorflow/component/facedetect-tensorflow/facedetect-tensorflow.component.ts
+++ src/app/features/facedetect/tensorflow/component/facedetect-tensorflow/facedetect-tensorflow.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, AfterViewInit, Renderer2, ViewChild } from '@angular/core';
-import * as tf from '@tensorflow/tfjs';
-import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as blazeface from '@tensorflow-models/blazeface';
+import '@tensorflow/tfjs-backend-webgl';
 
 @Component({
 	selector: 'ocv-facedetect-tensorflow',
@@ -29,30 +29,82 @@ export class OCVFacedetectTensorFlowComponent implements OnInit, AfterViewInit {
 	// Model Vars
 	model: any;
 	loading: boolean;
-	predictions: [
-		{
-			className: string;
-			probability: number;
-		}
-	];
 
 	constructor(private renderer: Renderer2) {}
 
 	ngOnInit() {
-		this.startCamera();
 		this.listCameras();
 		this.loadModel();
 	}
 
+	async ngAfterViewInit() {
+		this.startCamera();
+	}
+
 	async loadModel() {
 		this.loading = true;
-		this.model = await mobilenet.load();
+		this.model = await blazeface.load();
 		this.loading = false;
 
 		setInterval(async () => {
-			this.predictions = await this.model.classify(this.videoElement.nativeElement);
-			await tf.nextFrame();
-		}, 5000);
+			//blazeface
+			const returnTensors = false; // Pass in `true` to get tensors back, rather than values.
+			const predictions = await this.model.estimateFaces(this.videoElement.nativeElement, returnTensors);
+
+			this.renderer.setProperty(this.canvas.nativeElement, 'width', this.videoWidth);
+			this.renderer.setProperty(this.canvas.nativeElement, 'height', this.videoHeight);
+			this.canvas.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
+
+			if (predictions.length > 0) {
+				/*
+				`predictions` is an array of objects describing each detected face, for example:
+
+				[
+				{
+					topLeft: [232.28, 145.26],
+					bottomRight: [449.75, 308.36],
+					probability: [0.998],
+					landmarks: [
+					[295.13, 177.64], // right eye
+					[382.32, 175.56], // left eye
+					[341.18, 205.03], // nose
+					[345.12, 250.61], // mouth
+					[252.76, 211.37], // right ear
+					[431.20, 204.93] // left ear
+					]
+				}
+				]
+				*/
+
+				for (let i = 0; i < predictions.length; i++) {
+					const start = predictions[i].topLeft;
+					const end = predictions[i].bottomRight;
+					const size = [end[0] - start[0], end[1] - start[1]];
+
+					const eye_right = predictions[i].landmarks[0];
+					const eye_left = predictions[i].landmarks[1];
+
+					// Render a rectangle over each detected face.
+					var ctx = this.canvas.nativeElement.getContext('2d');
+					//ctx.globalAlpha = 0.3;
+					//ctx.fillStyle = 'blue';
+					//ctx.fillRect(start[0], start[1], size[0], size[1]);
+					this.drawdot(ctx, eye_right);
+					this.drawdot(ctx, eye_left);
+				}
+			}
+		}, 100);
+	}
+
+	drawdot(ctx, coords) {
+		ctx.beginPath();
+		ctx.arc(coords[0], coords[1], 70, 0, 2 * Math.PI);
+		ctx.globalAlpha = 0.3;
+		ctx.fill();
+		ctx.globalAlpha = 1;
+		ctx.lineWidth = 5;
+		ctx.strokeStyle = '#FFFFFF';
+		ctx.stroke();
 	}
 
 	startCamera() {
@@ -68,24 +120,6 @@ export class OCVFacedetectTensorFlowComponent implements OnInit, AfterViewInit {
 		} else {
 			alert('Sorry, camera not available.');
 		}
-	}
-
-	handleError(error) {
-		console.log('Error: ', error);
-	}
-
-	attachVideo(stream) {
-		this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
-		this.renderer.listen(this.videoElement.nativeElement, 'play', (event) => {
-			this.videoHeight = this.videoElement.nativeElement.videoHeight;
-			this.videoWidth = this.videoElement.nativeElement.videoWidth;
-		});
-	}
-
-	capture() {
-		this.renderer.setProperty(this.canvas.nativeElement, 'width', this.videoWidth);
-		this.renderer.setProperty(this.canvas.nativeElement, 'height', this.videoHeight);
-		this.canvas.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
 	}
 
 	listCameras() {
@@ -107,6 +141,18 @@ export class OCVFacedetectTensorFlowComponent implements OnInit, AfterViewInit {
 		});
 	}
 
+	handleError(error) {
+		console.log('Error: ', error);
+	}
+
+	attachVideo(stream) {
+		this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
+		this.renderer.listen(this.videoElement.nativeElement, 'play', (event) => {
+			this.videoHeight = this.videoElement.nativeElement.videoHeight;
+			this.videoWidth = this.videoElement.nativeElement.videoWidth;
+		});
+	}
+
 	changeCam() {
 		var select = this.select.nativeElement;
 		if (typeof this.currentStream !== 'undefined') {
@@ -121,22 +167,5 @@ export class OCVFacedetectTensorFlowComponent implements OnInit, AfterViewInit {
 			this.constraints.video.deviceId = { exact: select.value };
 		}
 		this.startCamera();
-	}
-
-	async ngAfterViewInit() {
-		/*
-		const vid = this.videoElement.nativeElement;
-
-		if (navigator.mediaDevices.getUserMedia) {
-			navigator.mediaDevices
-				.getUserMedia({ video: true })
-				.then((stream) => {
-					vid.srcObject = stream;
-				})
-				.catch((err0r) => {
-					console.log('Something went wrong!');
-				});
-		}
-		*/
 	}
 }
